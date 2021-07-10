811d1e4f4adbb659128dace7ae12f41bb8bfae2e
7506574455 <84278635+7506574455@users.noreply.github.com>
2021-07-10 01:53:02 -0700

    Refactor mood analyse program

--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -4,19 +4,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
-
-	    //TC1.1
-	    @Test
-	    public void givenMessage_WhenSad_ShouldReturnSad() {
-	        MoodAnalyzer moodAnalyser = new MoodAnalyzer();
-	        String mood = moodAnalyser.analyseMood("I am in Sad Mood");
-
-	    }
-        //TC1.2
-	    @Test
-	    public void givenMessage_WhenNotSad_ShouldReturnHappy() {
-	        MoodAnalyzer moodAnalyser = new MoodAnalyzer();
-	        String mood = moodAnalyser.analyseMood("I am in Any Mood");
-	        Assert.assertEquals("HAPPY", mood);
-	    }
+    
+	//TC1.1
+	@Test
+    public void givenMessage_WhenSad_ShouldReturnSad() {
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer("I am in Sad Mood");
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertEquals("SAD", mood);
+    }
+    //TC1.2
+    @Test
+    public void givenMessage_WhenNotSad_ShouldReturnHappy() {
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer("I am in Happy Mood");
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertEquals("HAPPY", mood);
+    }
 }
